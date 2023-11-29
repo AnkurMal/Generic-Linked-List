2c232acd86164b0b457fe468cd44ea75cc3a65bc
Ankur Mallick <108927188+AnkurMal@users.noreply.github.com>
2023-11-30 00:16:47 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef enum DataType
 {
-    INT, DOUBLE
+    CHAR, INT, DOUBLE, STRING
 }DataType;
 
 typedef struct Node
@@ -14,33 +15,39 @@ typedef struct Node
     DataType dataType;
 }Node;
 
+static void InsertData_(Node **list, void* dataPtr, DataType dataType);
+static void InsertChar(Node **list, char data);
 static void InsertInt(Node **list, int data);
 static void InsertDouble(Node **list, double data);
-static void InsertNumber(Node **list, void* dataPtr, DataType dataType);
+static void InsertString(Node **list, const char* data);
 static void PrintData(Node *list, size_t index);
-static void FreeList(Node *list);
 static void PrintList(Node *list);
+static void FreeList(Node *list);
 
 /**
  * Inserts the specified data at the end of the list.
  * @param listPtr pointer to the list (should be initialzed to NULL)
  * @param data data to be inserted to the list
+ * @note For a character data, it should be explicitely converted to char.
 */
 #define InsertData(listPtr, data) _Generic((data), \
+        char:   InsertChar, \
         int:    InsertInt, \
-        double: InsertDouble \
+        double: InsertDouble, \
+        char*:  InsertString \
     )(&listPtr, data)
 
 int main(void)
 {
     Node *list = NULL;
-    InsertData(list, -5);
+    InsertData(list, (char)'A');
     InsertData(list, 10);
     InsertData(list, 85.99);
     InsertData(list, 45);
     InsertData(list, 89);
     InsertData(list, 55.4789);
-    PrintData(list, 3);
+    InsertData(list, "hello");
+    //PrintData(list, 6);
     printf("The list is: ");
     PrintList(list);
     FreeList(list);
@@ -48,21 +55,35 @@ int main(void)
     return 0;
 }
 
+void InsertChar(Node **list, char data)
+{
+    char* dataPtr = malloc(sizeof(char));
+    *dataPtr = data;
+    InsertData_(list, (void*)dataPtr, CHAR);
+}
+
 void InsertInt(Node **list, int data)
 {   
     int* dataPtr = malloc(sizeof(int));
     *dataPtr = data;
-    InsertNumber(list, (void*)dataPtr, INT);
+    InsertData_(list, (void*)dataPtr, INT);
 }
 
 void InsertDouble(Node **list, double data)
 {   
     double* dataPtr = malloc(sizeof(double));
     *dataPtr = data;
-    InsertNumber(list, (void*)dataPtr, DOUBLE);
+    InsertData_(list, (void*)dataPtr, DOUBLE);
 }
 
-void InsertNumber(Node **list, void* dataPtr, DataType dataType)
+void InsertString(Node **list, const char* data)
+{
+    char* dataPtr = malloc(strlen(data)+1);
+    memcpy(dataPtr, data, strlen(data)+1);
+    InsertData_(list, (void*)dataPtr, STRING);
+}
+
+void InsertData_(Node **list, void* dataPtr, DataType dataType)
 {   
     Node *newNode = malloc(sizeof(Node));
 
@@ -85,6 +106,11 @@ void InsertNumber(Node **list, void* dataPtr, DataType dataType)
     (*list)->length++;
 }
 
+/**
+ * Prints the data at the specified index in the list.
+ * @param *list the list pointer
+ * @param index index of the data to be printed
+*/
 void PrintData(Node *list, size_t index)
 {
     for(size_t i=0; i<index; i++)
@@ -98,11 +124,21 @@ void PrintData(Node *list, size_t index)
         case DOUBLE:
             printf("%f\n", *(double*)list->data);
             break;
+        case STRING:
+            printf("%s\n", (char*)list->data);
+            break;
+        case CHAR:
+            printf("%c\n", *(char*)list->data);
+            break;
         default:
             break;
     }
 }
 
+/**
+ * Prints the entire list.
+ * @param *list the list pointer
+*/
 void PrintList(Node *list)
 {
     int c = 0, length = list->length;
@@ -125,6 +161,18 @@ void PrintList(Node *list)
                 else
                     printf("%f]\n", *(double*)list->data);
                 break;
+            case STRING:
+                if(c<length)
+                    printf("\"%s\", ", (char*)list->data);
+                else
+                    printf("\"%s\"]\n", (char*)list->data);
+                break;
+            case CHAR:
+                if(c<length)
+                    printf("\'%c\', ", *(char*)list->data);
+                else
+                    printf("\'%c\']\n", *(char*)list->data);
+                break;
             default:
                 break;
         }
@@ -132,6 +180,10 @@ void PrintList(Node *list)
     }    
 }
 
+/**
+ * Frees the enitre list.
+ * @param *list the list pointer
+*/
 void FreeList(Node *list)
 {
     Node *temp;
