2afb597af2022f1ca05449b68d1d2ccfaa25c7e0
Ankur Mallick <108927188+AnkurMal@users.noreply.github.com>
2023-12-26 00:03:32 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,24 +1,20 @@
 #include "linkedList.h"
 
 /*
-TODO: CAN I DIRECTLY PASS THE MALLOCED POINTER TO InsertData_ BY PASSING THE RETURNED void* TO dataPtr USING
+TODO: CAN I DIRECTLY PASS THE MALLOCED POINTER TO insertData_ BY PASSING THE RETURNED void* TO dataPtr USING
 FUNCTION POINTER ? DONT KNOW THE ADVANTAGE OF THIS THO
 */
+
 int main(void)
 {
     Node *list = NULL;
-
-    InsertData(list, (char)'A');
-    InsertData(list, 10);
-    InsertData(list, 85.99);
-    InsertData(list, 45);
-    InsertData(list, 89);
-    InsertData(list, 55.4789);
-    InsertData(list, "hello");
-    //PrintData(list, 7);
+    insertData(list, 5);
+    insertData(list, (char)'A');
+    insertDataAt(list, 56, 1);
+    insertData(list, "hello");
     printf("The list is: ");
-    PrintList(list);
-    FreeList(list);
+    printList(list);
+    freeList(list);
     
     return 0;
 }
