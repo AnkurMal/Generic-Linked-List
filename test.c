0c633bbfafe27fc4aa43f9f48810112233c45713
Ankur Mallick <108927188+AnkurMal@users.noreply.github.com>
2024-01-14 23:12:43 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,11 +1,6 @@
 #include "linkedList.h"
 #include <stdio.h>
 
-/*
-TODO: CAN I DIRECTLY PASS THE MALLOCED POINTER TO insertData_ BY PASSING THE RETURNED void* TO dataPtr USING
-FUNCTION POINTER ? DONT KNOW THE ADVANTAGE OF THIS THO
-*/
-
 int main(void)
 {
     Node *list = initList();
